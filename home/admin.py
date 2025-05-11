d31fe36c3f9b1e8be0323dad854f2368f8bf3119
sufian-sani <abu5suffian@gmail.com>
2025-05-12 01:39:15 +0600

    first commit

---
+++ home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# from home.models import Post
+
+# Register your models here.
+
+# admin.site.register(Post)
---
+++ home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+# class Post(models.Model):
+#     post_title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     image = models.ImageField(upload_to='post_images/')
+#     create_date = models.DateTimeField(default=timezone.now)
+
+#     def __str__(self):
+#         return self.post_title
